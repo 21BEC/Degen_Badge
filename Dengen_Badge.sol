3482b2bb3dfc2a15b906a84ade63e7efb2670f99
Mradul <94035599+21BEC@users.noreply.github.com>
2023-07-23 14:33:49 +0530

    Dengen_Badge.sol

--- Dengen_Badge.sol
+++ Dengen_Badge.sol
@@ -1,43 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.3.0/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Dengen_Badge is ERC20 {
-    address private owner;
+contract Dengen_Badge is ERC20, Ownable {
+    
+    mapping(string => uint256) private _itemPrices;
 
-    constructor() ERC20("Degen Coin", "DGN") {
-        owner = msg.sender;
+    event ItemRedeemed(address indexed player, string item);
+
+    constructor() ERC20("DENGEN COIN", "DGN") {
+        // Mint initial supply to the contract deployer (owner)
+        _mint(msg.sender, 1000000 * 5**decimals());
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Owner is only able to access this");
-        _;
-    }
-
+    // Function to mint new tokens (only the owner can do this)
     function mintTokens(address account, uint256 amount) public onlyOwner {
         _mint(account, amount);
+        emit Transfer(address(0), account, amount);
     }
-    
+
+    function redeemItem(string memory item) public {
+        require(_itemPrices[item] > 0, "Item not available for redemption");
+        require(balanceOf(msg.sender) >= _itemPrices[item], "Insufficient balance");
+
+        // Perform the redemption (in this example, transfer the tokens to the contract owner)
+        _transfer(msg.sender, owner(), _itemPrices[item]);
+
+        emit ItemRedeemed(msg.sender, item);
+    }
+
+    // Function to add items and their prices
+    function addItemToStore(string memory item, uint256 price) public onlyOwner {
+        require(price > 0, "Price must be greater than zero");
+        _itemPrices[item] = price;
+    }
+
+    function getItemPrice(string memory item) public view returns (uint256) {
+        return _itemPrices[item];
+    }
+
     function burnTokens(uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+
         _burn(msg.sender, amount);
-    }
-
-    function redeem(uint256 amount) public {
-        _burn(msg.sender, amount);
-    }
-
-    function transferTokens(address recipient, uint256 amount) public returns (bool) {
-        _transfer(_msgSender(), recipient, amount);
-        return true;
-    }
-
-    function getbalance(address account) public view  returns (uint256) {
-        return super.balanceOf(account);  
-        //it is already built in ERC20 that's why we are able to call it
-    }
-
-    function getOwner() public view returns (address) {
-        return owner;
     }
 }
